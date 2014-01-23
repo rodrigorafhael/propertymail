42d55838a08eb3bebf8c035b2fb4dccaea5c7413
Rodrigo Rafhael <rodrigorafhael@gmail.com>
2014-01-23 17:45:58 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Propertymail::Application.config.secret_token = '6ef0d43ed4d964d7d2dd09b52fa1ec7c71380799f091955548c893696248eadc3c39b6f6b8a6a41a37c4ad4b152b390917d705ade287dec9960c8aa18e161002'
